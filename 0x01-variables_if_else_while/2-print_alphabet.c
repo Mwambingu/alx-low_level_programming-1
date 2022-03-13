f34918afcb0af9384d129b908b38f0f058de2c1f
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-13 15:59:55 +0300

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase
+* Return: 0 for success
+*/
+int main(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+
+	putchar(letter);
+	 letter++;
+
+		putchar ('\n');
+
+	return (0);
+}
