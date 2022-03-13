6a5f81d7ff88c662b26e0990771c487a633f2310
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-13 13:53:18 +0300

    THE LAST DIGIT

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  * Description: prints "Programming is like building a multilingual puzzle"
-  * Return: 0
- */
+ * main - Entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
 
 int main(void)
 {
@@ -15,7 +15,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	last_d = n % 10;
 	if (last_d > 5)
 	{
 		printf("Last digit of %i is %d and is greater than 5\n", n, last_d);
@@ -24,7 +24,7 @@ int main(void)
 	{
 		printf("Last digit of %i is %d and is 0\n", n, last_d);
 	}
-	else if (last_digit < 6 && last_d != 0)
+	else if (last_d < 6 && last_d != 0)
 	{
 		printf("Last digit of %i is %d and is less than 6 and not 0\n", n, last_d);
 	}
