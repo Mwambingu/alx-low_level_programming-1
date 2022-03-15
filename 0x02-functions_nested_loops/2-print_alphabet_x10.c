f1cae384f1744bf72766ac09f86008a2bc7174ef
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-15 11:40:55 +0300

    10x AlphabeT

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,22 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Entry point
- * Description: Print the alphabet in lowercase 10 times
- * followed by a new line
+ * print_alphabet_x10 - entry point
+ * Description: Print lowercase alphabet 10 times
  * Return: 0 for success
  */
 
-
 void print_alphabet_x10(void)
 {
+	char ch;
+	int i;
 
-	int x, a;
-	int lower_x;
-
-	for (a = 0; a <= 9; a++)
+	for (i = 0; i < 10; i++)
 	{
-		for (x = 'A'; x <= 'Z'; x++)
-		{
-			lower_x = tolower(x);
-			_putchar(lower_x);
-		}
+
+	for (ch = 'a'; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
 		_putchar('\n');
 	}
 }
