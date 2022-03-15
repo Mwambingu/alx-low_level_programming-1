c1229e207e7b2b6d92bc5a4605449596bfea1348
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-15 23:03:02 +0300

    JACK BAUER

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,13 +3,14 @@
 /**
  * jack_bauer -  function that prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59.
- * Return: nothing
+ * Return: 0 o 1 depending on the answer
  */
 
 void jack_bauer(void)
 {
 int hr;
 int min;
+
 for (hr = 0; hr <= 24; hr++)
 {
 for (min = 0; min <= 60; min++)
