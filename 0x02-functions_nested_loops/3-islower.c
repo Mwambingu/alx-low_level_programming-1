36034dafd189558734f81e162bb1f35b31d05ff3
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-15 11:55:39 +0300

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check if letter c is in lowercase
+ * @c: letter to be checked
+ * Return: 1 if lower, 0 if capital
+ */
+
+int _islower(int c)
+{
+if islower(c)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
