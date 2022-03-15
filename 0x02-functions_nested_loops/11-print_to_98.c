faf6981845ecebcc582bfad91bb8a0fd92434a92
Lilywd <ndungulilianwanjiku@gmail.com>
2022-03-16 00:47:16 +0300

    98batterystreet

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98
- * followed by a nwe line
- * @n: print from this number
+ * print_to_98- A fucntion that prints integers from
+ * 0 to 98
+ * Return: no return
+ * @n: integer
  */
 
 void print_to_98(int n)
 {
-
-if (n >= 98)
+if (n <= 98)
 {
-while (n > 98)
-printf("%d, ", n--);
-printf("%d\n, n);
+for (n = n; n <= 97; n++)
+printf("%d, ", n);
 }
-
 else
 {
-while (n < 98)
-printf("%d, ", n++);
-printf("%d\n", n);
+for (n = n; n > 98; n--)
+printf("%d, ", n);
 
 }
+printf("%d\n", 98);
 }
 
